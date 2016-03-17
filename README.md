4bd13052bd8d5d7b2aff07ba2a5c3bc0b1223035
Matias Gordon <matias.gordon@mercadolibre.com>
2016-03-17 14:56:32 -0300

    v1.0.0 (Basic checkout)

--- README.md
+++ README.md
@@ -1,3 +1,76 @@
-# WooCommerce - MercadoPago Cart
+# WooCommerce - MercadoPago Module (v1.0.0 - first release)
+---
 
-Coming soon...
+* [Features](#features)
+* [Available versions](#available_versions)
+* [Installation](#installation)
+* [Configuration](#configuration)
+
+<a name="features"></a>
+##Features##
+**Standard checkout**
+
+This feature allows merchants to have a standard checkout. It includes features like
+customizations of title, description, category, and external reference, integrations via
+iframe, modal, and redirection, with configurable auto-returning, max installments and
+payment method exclusion setup, and sandbox/debug options.
+
+*Available for Argentina, Brazil, Chile, Colombia, Mexico and Venezuela*
+
+<a name="available_versions"></a>
+##Available versions##
+<table>
+  <thead>
+    <tr>
+      <th>Plugin Version</th>
+      <th>Status</th>
+      <th>Compatible Versions</th>
+    </tr>
+  <thead>
+  <tbody>
+    <tr>
+      <td>v1.0.0</td>
+      <td>Actual (Stable)</td>
+      <td>WooCommerce 2.1.x - 2.5.x</td>
+    </tr>
+  </tbody>
+</table>
+
+<a name="installation"></a>
+##Installation##
+
+1. Copy **cart-woocommerce/mercadopago** folder to **[WordPressRootDirectory]/wp-content/plugins/** folder.
+
+2. On your store administration, go to **Plugins** item in sidebar.
+
+3. Search by **WooCommerce Mercado Pago** and click enable. <br />
+You will receive the following message: "Plugin enabled." as a notice in your WordPress.
+
+<a name="configuration"></a>
+##Configuration##
+
+1. Go to **WooCommerce > Configuration > Checkout Tab > Mercado Pago**.
+
+2. Set your **Client_id**, **Client_secret** accordingly to your country:
+
+	* Argentina: https://www.mercadopago.com/mla/herramientas/aplicaciones
+	* Brazil: https://www.mercadopago.com/mlb/ferramentas/aplicacoes
+	* Chile: https://www.mercadopago.com/mlc/herramientas/aplicaciones
+	* Colombia: https://www.mercadopago.com/mco/herramientas/aplicaciones
+	* Mexico: https://www.mercadopago.com/mlm/herramientas/aplicaciones
+	* Venezuela: https://www.mercadopago.com/mlv/herramientas/aplicaciones
+
+2. Other general configurations:<br />
+	* **Instant Payment Notification (IPN) URL**: The highlighted URL is where you will get notified about payment updates;
+	* **Title**: This is the title of the payment option that will be shown to your customers;
+	* **Description**: This is the description of the payment option that will be shown to your customers;
+	* **Store Category**: Sets up the category of the store;
+	* **Store Identificator**: A prefix to identify your store, when you have multiple stores for only one Mercado Pago account;
+	* **Integration Method**: How your customers will interact with Mercado Pago to pay their orders;
+	* **iFrame Width**: The width, in pixels, of the iFrame (used only with iFrame Integration Method);
+	* **iFrame Height**: The height, in pixels, of the iFrame (used only with iFrame Integration Method);
+	* **Auto Return**: If set, the platform will return to your store when the payment is approved;
+	* **Max Installments**: The maximum installments allowed for your customers;
+	* **Exclude Payment Methods**: Select the payment methods that you want to not work with Mercado Pago;
+	* **Mercado Pago Sandbox**: Test your payments in Mercado Pago sandbox environment;
+	* **Debug and Log**: Enables/disables system logs.
---
+++ mercadopago/mercadopago-module.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Mercado Pago
+ * Plugin URI: https://github.com/mercadopago/cart-woocommerce
+ * Description: This is the <strong>oficial</strong> module of Mercado Pago for WooCommerce plugin. This module enables WooCommerce to use Mercado Pago as a payment Gateway for purchases made in your e-commerce store.
+ * Author: Mercado Pago
+ * Author URI: https://developers.mercadopago.com/
+ * Developer: Marcelo Tomio Hama / marcelo.hama@mercadolivre.com
+ * Copyright: Copyright(c) MercadoPago [https://www.mercadopago.com]
+ * Version: 1.0.0
+ * License: http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
+ * Text Domain: woocommerce-mercadopago-module
+ * Domain Path: /languages/
+ */
+
+/**
+ * Implementation references:
+ * 1. https://docs.woothemes.com/document/payment-gateway-api/
+ * 2. https://www.mercadopago.com.br/developers/en/api-docs/
+ */
+ 
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+// Check if class is already loaded
+if (!class_exists('WC_WooMercadoPago_Module')) :
+
+/*
+ * WooCommerce MercadoPago Module main class
+ */
+class WC_WooMercadoPago_Module {
+
+	// Singleton design pattern
+	protected static $instance = null;
+	public static function initMercadoPagoGatewayClass() {
+		if (null == self::$instance) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
+
+	// Class constructor
+	private function __construct() {
+		// load plugin text domain
+		add_action('init', array($this, 'load_plugin_textdomain'));
+		// verify if WooCommerce is already installed
+		if (class_exists('WC_Payment_Gateway')) {
+			include_once 'mercadopago/mercadopago-gateway.php';
+			add_filter('woocommerce_payment_gateways', array( $this, 'addGateway'));
+		} else {
+			add_action('admin_notices', array($this, 'notifyWooCommerceMiss'));
+		}
+	}
+	
+	// As well as defining your class, you need to also tell WooCommerce (WC) that
+	// it exists. Do this by filtering woocommerce_payment_gateways.
+	public function addGateway($methods) {
+		$methods[] = 'WC_WooMercadoPago_Gateway';
+		return $methods;
+	}
+	
+	// Places a warning error to notify user that WooCommerce is missing
+	public function notifyWooCommerceMiss() {
+		echo
+			'<div class="error"><p>' .
+			sprintf(
+				__('WooCommerce Mercado Pago Oficial depends on the last version of %s to execute!', 'woocommerce-mercadopago-module'),
+				'<a href="http://wordpress.org/extend/plugins/woocommerce/">' . 'WooCommerce' . '</a>'
+			) .
+			'</p></div>';
+	}
+	
+	// IPN compatibility with version prior to 2.1
+	public static function woocommerceInstance() {
+		if (function_exists('WC')) {
+			return WC();
+		} else {
+			global $woocommerce;
+			return $woocommerce;
+		}
+	}
+	
+	// Multi-language plugin
+	public function load_plugin_textdomain() {
+		$locale = apply_filters('plugin_locale', get_locale(), 'woocommerce-mercadopago-module');
+		load_textdomain('woocommerce-mercadopago-module', trailingslashit(WP_LANG_DIR ) . 'woocommerce-mercadopago-module/woocommerce-mercadopago-module-' . $locale . '.mo');
+		load_plugin_textdomain('woocommerce-mercadopago-module', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+	}
+	
+}
+	
+// Payment gateways should be created as additional plugins that hook into WooCommerce.
+// Inside the plugin, you need to create a class after plugins are loaded
+add_action('plugins_loaded', array('WC_WooMercadoPago_Module', 'initMercadoPagoGatewayClass'), 0);
+
+// Support to previous IPN implementations
+function wcmercadopago_legacy_ipn() {
+	if (isset($_GET['topic']) && !isset($_GET['wc-api'])) {
+		$woocommerce = WC_WooMercadoPago_Module::woocommerceInstance();
+		$woocommerce->payment_gateways();
+		do_action('woocommerce_api_wc_woomercadopago_gateway');
+	}
+}
+add_action('init', 'wcmercadopago_legacy_ipn');
+
+endif;
+
+?>
