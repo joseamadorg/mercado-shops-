1572c18981fe6807c5bdef354df9d10339a6414d
Marcelo Hama <mhama@mac-mhama.local>
2016-06-14 18:43:24 -0300

    fix in url link for peru when seeking credentials

--- woo-mercado-pago-module/mercadopago/mercadopago-gateway.php
+++ woo-mercado-pago-module/mercadopago/mercadopago-gateway.php
@@ -131,7 +131,7 @@ class WC_WooMercadoPago_Gateway extends WC_Payment_Gateway {
 			'<a href="https://www.mercadopago.com/mlc/herramientas/aplicaciones" target="_blank">%s</a>, ' .
 			'<a href="https://www.mercadopago.com/mco/ferramentas/aplicacoes" target="_blank">%s</a>, ' .
 			'<a href="https://www.mercadopago.com/mlm/ferramentas/aplicacoes" target="_blank">%s</a>, ' .
-			'<a href="https://www.mercadopago.com/mpe/herramientas/aplicaciones" target="_blank">%s</a> %s ' .
+			'<a href="https://www.mercadopago.com/mpe/account/credentials?type=basic" target="_blank">%s</a> %s ' .
 			'<a href="https://www.mercadopago.com/mlv/herramientas/aplicaciones" target="_blank">%s</a>',
 			__( 'Argentine', 'woocommerce-mercadopago-module' ),
 			__( 'Brazil', 'woocommerce-mercadopago-module' ),
