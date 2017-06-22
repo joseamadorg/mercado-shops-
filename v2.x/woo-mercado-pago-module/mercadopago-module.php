8b59b920a6e836ee35e5e673b466b651342e5647
Marcelo Tomio Hama <hama.marcelo.ti@gmail.com>
2017-06-21 22:07:12 -0300

    error log api integrated

--- v2.x/woo-mercado-pago-module/mercadopago-module.php
+++ v2.x/woo-mercado-pago-module/mercadopago-module.php
@@ -31,7 +31,7 @@ if ( ! class_exists( 'WC_WooMercadoPago_Module' ) ) :
 	 */
 	class WC_WooMercadoPago_Module {
 
-		const VERSION = '2.2.7';
+		const VERSION = '2.2.8';
 
 		// Singleton design pattern
 		protected static $instance = null;
